4df52702ad0610e5f31c883e14e65000e1688144
Joshua Jiang <Joshua.Jiang@outlook.com>
2015-07-12 18:04:59 +0800

    checkin

--- src/main/java/joshua/leetcode/array/MajorityElement2.java
+++ src/main/java/joshua/leetcode/array/MajorityElement2.java
@@ -1,7 +1,29 @@
 package joshua.leetcode.array;
 
+import java.util.List;
+
 /**
- * Created by joy on 2015/7/12.
+ * 	229	Majority Element II
+ *
+ * 	@see <a href="https://leetcode.com/problems/majority-element-ii/">leetcode link</a>
  */
-public class MajorityElement2 {
+public abstract class MajorityElement2 {
+
+    /**
+     * Given an integer array of size n, find all elements that appear more than ⌊n/3⌋ times.
+     *
+     * The algorithm should run in linear time and in O(1) space.
+     *
+     * @param nums
+     * @return
+     */
+    public abstract List<Integer> majorityElement(int[] nums);
+
+    static class Solution1 extends  MajorityElement2{
+
+        @Override
+        public List<Integer> majorityElement(int[] nums) {
+
+        }
+    }
 }
