bf5044cb84fa344320d5a88d8bbfc335cc35a02d
Joshua Jiang <Joshua.Jiang@outlook.com>
2016-05-22 17:33:34 +0800

    Find Celebrity

---
+++ src/main/java/joshua/leetcode/array/FindCelebrity.java
@@ -0,0 +1,13 @@
+package joshua.leetcode.array;
+
+/**
+ * 277. Find the Celebrity<br/>
+ *
+ * <a href="https://leetcode.com/problems/find-the-celebrity/">leetcode link</a>
+ *
+ * @author Joshua.Jiang on 2016/5/22.
+ */
+public abstract class FindCelebrity {
+
+    
+}
